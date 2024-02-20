5956d3372e27b9c1f290ffc25ad9842498951cc5
Christoffer Rydeståhl <kristoffer@oholics.com>
2024-02-20 22:18:41 +0100

    styled the desktop nav.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,16 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from "astro-icon";
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), compress()],
+  }), compress(), react()],
   output: "server",
   adapter: netlify()
 });
